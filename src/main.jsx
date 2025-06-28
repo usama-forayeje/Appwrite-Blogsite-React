ab244412d39ac794bff2201508e95742d73cde26
usama_forayaje <usamaforayaje03@gmail.com>
2025-06-28 06:17:55 +0600

    project init with react query and shadcn ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,82 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
-import store from "./store/store.js";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "./pages/Home.jsx"; // Adjust the path as necessary
-import AuthLayout from "./components/AuthLayout.jsx";
-import SignIn from "./pages/SignIn.jsx"; // Adjust the path as necessary
-import AllPosts from "./pages/AllPosts.jsx"; // Adjust the path as necessary
-import AddPost from "./pages/AddPost.jsx"; // Adjust the path as necessary
-import EditPost from "./pages/EditPost.jsx"; // Adjust the path as necessary
-import Post from "./pages/Post.jsx"; // Adjust the path as necessary
-import LogInPage from "./pages/LogInPage.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "logIn",
-        element: (
-          <AuthLayout Authenticated={false}>
-            <LogInPage />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/signup",
-        element: (
-          <AuthLayout Authenticated={false}>
-            <SignIn />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/allPosts",
-        element: (
-          <AuthLayout Authenticated>
-            {" "}
-            <AllPosts />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/add-post",
-        element: (
-            <AuthLayout Authenticated>
-                {" "}
-                <AddPost />
-            </AuthLayout>
-        ),
-    },
-    {
-      path: "/edit-post/:slug",
-      element: (
-          <AuthLayout authentication>
-              {" "}
-              <EditPost />
-          </AuthLayout>
-      ),
-  },
-  {
-      path: "/post/:slug",
-      element: <Post />,
-  },
-    ],
-  },
-]);
-createRoot(document.getElementById("root")).render(
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </StrictMode>
-);
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>,
+)
